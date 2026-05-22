8c5576dff5643aaaf60705893e780142f65fa388
Sahid Reza <sahidreza976@gmail.com>
2026-05-22 07:45:58 +0530

    Added Java source files

---
+++ src/main/java/org/example/Array/array_1.java
@@ -0,0 +1,20 @@
+package org.example.Array;
+
+public class array_1 {
+
+        public static void main(String[] args) {
+            int marks [] = new int[3];
+            marks[0]= 89;
+            marks[1]= 81;
+            marks[2]= 72;
+
+            for(int i = 0;i<3;i++){
+                System.out.println(marks[i]);
+            }
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+        }
+    }
+
+
