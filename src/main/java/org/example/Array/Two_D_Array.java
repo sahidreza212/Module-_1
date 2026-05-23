466855f31971c4ca0bb091eb047bc3a2fc5014b4
Sahid Reza <sahidreza976@gmail.com>
2026-05-23 07:36:24 +0530

    2D_Array

---
+++ src/main/java/org/example/Array/Two_D_Array.java
@@ -0,0 +1,35 @@
+package org.example.Array;
+
+import java.util.Scanner;
+
+public class Two_D_Array {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            int rows = sc.nextInt();
+            int cols = sc.nextInt();
+
+            int [] [] numbers = new int[rows][cols];
+
+            for(int i = 0; i<rows;i++){
+                for (int j = 0;j<cols;j++){
+                    numbers [i][j] = sc.nextInt();
+                }
+            }
+
+            int x = sc.nextInt();
+
+            for (int i = 0; i<rows;i++){
+                for (int j = 0; j<cols;j++){
+                    // System.out.print(numbers[i][j] + " ");
+                    if(numbers[i][j] == x){
+                        System.out.println(" x found at the location :(" +i+","+j+")");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
