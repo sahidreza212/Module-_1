0b3b1e41d1f6eb156f3545714ca103efcc2b5d0a
Sahid Reza <sahidreza976@gmail.com>
2026-05-23 07:40:57 +0530

    String_2

---
+++ src/main/java/org/example/Array/String_2.java
@@ -0,0 +1,39 @@
+package org.example.Array;
+
+public class String_2 {
+
+        public static void main(String[] args) {
+
+            StringBuilder sb = new StringBuilder("S");
+            sb.append("a");
+            sb.append("h");
+            sb.append("i");
+            sb.append("d");
+            System.out.println(sb);
+
+        /*
+        System.out.println(sb);
+
+        // ChatAt --->
+        System.out.println(sb.charAt(0));
+
+        //Set Chat At Index
+        sb.setCharAt(0,'N');
+        System.out.println(sb);
+
+        //Insert Char
+        sb.insert(0,'D');
+        System.out.println(sb);
+
+        //Delete char
+        sb.delete(0,1);
+        System.out.println(sb);
+
+
+
+         */
+
+        }
+    }
+
+
